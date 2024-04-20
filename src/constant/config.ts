3bd9759a92d6d93d3d62402df1ebb05cf2cff0bb
dimasandriano <dimas.andriano.h@gmail.com>
2024-04-20 19:00:01 +0700

    boilerplate: clean config, component, file etc

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,6 @@
-export const siteConfig = {
+export const siteConfig: { title: string; description: string; url: string } = {
   title: 'Next.js + Tailwind CSS + TypeScript Starter',
   description:
     'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://tsnext-tw.thcl.dev',
 };
--- src/lib/og.ts
+++ src/lib/og.ts
@@ -1,18 +1,16 @@
+import { siteConfig } from '@/constant/config';
+
 type OpenGraphType = {
   siteName: string;
   description: string;
   templateTitle?: string;
   logo?: string;
 };
-// !STARTERCONF This OG is generated from https://github.com/theodorusclarence/og
-// Please clone them and self-host if your site is going to be visited by many people.
-// Then change the url and the default logo.
 export function openGraph({
   siteName,
   templateTitle,
   description,
-  // !STARTERCONF Or, you can use my server with your own logo.
-  logo = 'https://og.<your-domain>/images/logo.jpg',
+  logo = siteConfig.url + '/images/logo.jpg',
 }: OpenGraphType): string {
   const ogLogo = encodeURIComponent(logo);
   const ogSiteName = encodeURIComponent(siteName.trim());
@@ -21,7 +19,10 @@ export function openGraph({
     : undefined;
   const ogDesc = encodeURIComponent(description.trim());
 
-  return `https://og.<your-domain>/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
-    ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
-  }`;
+  return (
+    siteConfig.url +
+    `/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+      ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
+    }`
+  );
 }
