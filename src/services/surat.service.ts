cdfc952d00e0de356104265688582dd721d6e13d
dimasandriano <dimas.andriano.h@gmail.com>
2024-05-18 16:28:34 +0700

    fix: add token in service surat

--- src/services/surat.service.ts
+++ src/services/surat.service.ts
@@ -1,6 +1,10 @@
+import Cookies from 'js-cookie';
+
 import instance from '@/lib/axios/instance';
 
 import { TParams } from '@/types/params.type';
+
+const token = Cookies.get('token');
 
 type TParamsSurat = TParams & {
   status?: string;
@@ -12,6 +16,9 @@ export const suratService = {
   getAllSurat: async (params: TParamsSurat) => {
     const { data } = await instance.get('/surat/list', {
       params,
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
     });
     return data;
   },
