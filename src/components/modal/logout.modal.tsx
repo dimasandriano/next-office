4d73fef63fa9df4993530586579de45838391da7
dimasandriano <dimas.andriano.h@gmail.com>
2024-05-25 10:30:29 +0700

    feat: logout

---
+++ src/components/modal/logout.modal.tsx
@@ -0,0 +1,59 @@
+'use client';
+import Cookies from 'js-cookie';
+import { ArrowRight } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import toast from 'react-hot-toast';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+export default function LogoutModal() {
+  const router = useRouter();
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button variant='outline' className='w-full'>
+          <div className='flex items-center gap-3'>
+            Logout
+            <ArrowRight />
+          </div>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className='mb-5'>
+          <DialogTitle>Apakah anda yakin untuk keluar?</DialogTitle>
+        </DialogHeader>
+        <DialogFooter className='flex justify-between'>
+          <DialogClose asChild>
+            <Button type='button' variant='secondary' className='flex-1'>
+              Tidak
+            </Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button
+              type='button'
+              variant='destructive'
+              className='flex-1'
+              onClick={() => {
+                Cookies.remove('token');
+                toast.success('Logout Berhasil');
+                router.replace('/login');
+              }}
+            >
+              Keluar
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/molecules/Sidebar.tsx
+++ src/components/molecules/Sidebar.tsx
@@ -9,49 +9,53 @@ import {
 import Link from 'next/link';
 import React from 'react';
 
+import LogoutModal from '@/components/modal/logout.modal';
 import { Button } from '@/components/ui/button';
 
 export default function Sidebar() {
   return (
     <div className='h-screen w-full space-y-5 p-3'>
       <div className='pb-2 text-center'>LOGO</div>
-      <div className='flex flex-col justify-start gap-5'>
-        <Button variant='outline' asChild isSidebar>
-          <Link href='/e-surat' className='flex items-center gap-3'>
-            <LayoutDashboard />
-            Dashboard
-          </Link>
-        </Button>
-        <Button variant='outline' asChild isSidebar>
-          <Link href='/e-surat/surat' className='flex items-center gap-3'>
-            <Mail />
-            Kelola Surat
-          </Link>
-        </Button>
-        <Button variant='outline' asChild isSidebar>
-          <Link href='/e-surat/surat' className='flex items-center gap-3'>
-            <ScrollText />
-            Kelola Lamaran
-          </Link>
-        </Button>
-        <Button variant='outline' asChild isSidebar>
-          <Link href='/e-surat/kategori' className='flex items-center gap-3'>
-            <SquareStack />
-            Kelola Kategori
-          </Link>
-        </Button>
-        <Button variant='outline' asChild isSidebar>
-          <Link href='/e-surat/divisi' className='flex items-center gap-3'>
-            <Split />
-            Kelola Divisi
-          </Link>
-        </Button>
-        <Button variant='outline' asChild isSidebar>
-          <Link href='/e-surat/users' className='flex items-center gap-3'>
-            <Users />
-            Kelola Pengguna
-          </Link>
-        </Button>
+      <div className='flex h-[calc(100vh-75px)] flex-col justify-between'>
+        <div className='flex flex-col justify-start gap-5'>
+          <Button variant='outline' asChild isSidebar>
+            <Link href='/e-surat' className='flex items-center gap-3'>
+              <LayoutDashboard />
+              Dashboard
+            </Link>
+          </Button>
+          <Button variant='outline' asChild isSidebar>
+            <Link href='/e-surat/surat' className='flex items-center gap-3'>
+              <Mail />
+              Kelola Surat
+            </Link>
+          </Button>
+          <Button variant='outline' asChild isSidebar>
+            <Link href='/e-surat/surat' className='flex items-center gap-3'>
+              <ScrollText />
+              Kelola Lamaran
+            </Link>
+          </Button>
+          <Button variant='outline' asChild isSidebar>
+            <Link href='/e-surat/kategori' className='flex items-center gap-3'>
+              <SquareStack />
+              Kelola Kategori
+            </Link>
+          </Button>
+          <Button variant='outline' asChild isSidebar>
+            <Link href='/e-surat/divisi' className='flex items-center gap-3'>
+              <Split />
+              Kelola Divisi
+            </Link>
+          </Button>
+          <Button variant='outline' asChild isSidebar>
+            <Link href='/e-surat/users' className='flex items-center gap-3'>
+              <Users />
+              Kelola Pengguna
+            </Link>
+          </Button>
+        </div>
+        <LogoutModal />
       </div>
     </div>
   );
