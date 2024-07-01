01abd39c1b78ce297dbff9d72e54af6ded97b154
dimasandriano <dimas.andriano.h@gmail.com>
2024-07-02 01:29:39 +0700

    chore: hide list users != superadmin

--- src/components/molecules/Sidebar.tsx
+++ src/components/molecules/Sidebar.tsx
@@ -11,11 +11,14 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 
+import { useDecodedTokenJWTClient } from '@/hooks/useDecodedTokenJWT';
+
 import LogoutModal from '@/components/modal/logout.modal';
 import { Button } from '@/components/ui/button';
 
 export default function Sidebar() {
   const pathname = usePathname();
+  const decoded = useDecodedTokenJWTClient();
   return (
     <div className='h-screen w-full space-y-5 p-3'>
       <div className='text-center text-2xl font-bold'>E-SURAT</div>
@@ -71,16 +74,18 @@ export default function Sidebar() {
               Kelola Divisi
             </Link>
           </Button>
-          <Button
-            variant={pathname === '/e-surat/users' ? 'default' : 'outline'}
-            asChild
-            isSidebar
-          >
-            <Link href='/e-surat/users' className='flex items-center gap-3'>
-              <Users />
-              Kelola Pengguna
-            </Link>
-          </Button>
+          {decoded?.role === 'superadmin' && (
+            <Button
+              variant={pathname === '/e-surat/users' ? 'default' : 'outline'}
+              asChild
+              isSidebar
+            >
+              <Link href='/e-surat/users' className='flex items-center gap-3'>
+                <Users />
+                Kelola Pengguna
+              </Link>
+            </Button>
+          )}
         </div>
         <LogoutModal />
       </div>
